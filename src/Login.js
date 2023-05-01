f5b00f7f967530096f2c9fa6c28a52ba381ce1f9
Hassan Rasheed <hassanrasheed@Hassans-MacBook-Pro.local>
2023-05-01 22:04:30 +0200

    Add login using google and auth error messages

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { auth, signInWithEmailAndPassword, RecaptchaVerifier } from './firebaseConfig';
-// import { signInWithEmailAndPassword, RecaptchaVerifier, signOut } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
 import SignUpForm from './SignUpForm';
 
 const Login = () => {
@@ -9,6 +10,28 @@ const Login = () => {
   const [showSignUpForm, setShowSignUpForm] = useState(false);
   const [recaptchaContainer, setRecaptchaContainer] = useState(null);
   const [recaptchaVerifier, setRecaptchaVerifier] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const errorMessages = {
+    'auth/user-not-found': 'User not found. Please check your email and try again.',
+    'auth/wrong-password': 'Incorrect password. Please try again.',
+    'auth/invalid-email': 'Invalid email address. Please check your email and try again.',
+    'auth/too-many-requests': 'Too many unsuccessful login attempts. Please try again later.',
+    // Add other error codes and messages as needed
+  };
+  
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // User signed in with Google
+        console.log('User signed in with Google:', result.user);
+      })
+      .catch((error) => {
+        console.error('Error signing in with Google:', error);
+        setErrorMessage(error);
+      });
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -25,6 +48,7 @@ const Login = () => {
       console.log("User signed in:", userCredential.user);
     } catch (error) {
       console.error("Error signing in:", error);
+      setErrorMessage(errorMessages[error.code] || error.message);
     }
   };
 
@@ -57,6 +81,8 @@ const Login = () => {
           Sign Up
         </button>
       </form>
+      <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
       <div ref={setRecaptchaContainer}></div>
     </div>
   );
